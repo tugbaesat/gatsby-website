e3b074cea40a77444949470d2d28201677a95667
Tugba Esat <tugbaesat007@gmail.com>
2024-01-30 15:41:00 -0800

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,12 +1,16 @@
 import * as React from "react";
 import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
 
 const BlogPost = ({ data, children }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image);
+
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
       {children}
     </Layout>
   );
@@ -17,6 +21,15 @@ export const query = graphql`
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
+        hero_image {
+          id
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
       }
     }
   }
